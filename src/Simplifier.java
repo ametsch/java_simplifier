4f468ea3f88a42f7edd466611d57c718b9c46cbc
Aaron <aaron1@metschnet.com>
2021-03-04 09:22:27 -0500

    add value of pi

--- src/Simplifier.java
+++ src/Simplifier.java
@@ -19,6 +19,8 @@ public class Simplifier {
 
     public static final double doubleUpperLimit = 1.7e+308d;
     public static final double doubleLowerLimit = 1.7e-308d;
+
+    public static final double pi = 3.14159265358979d;
 //----------------------------------------------------------------------------------------------------------------------
     public static void p(String a){
         System.out.print(a);
