41f1fcec88fd30da9e502c2b6d75ca208268eae0
Aaron <aaron1@metschnet.com>
2021-03-16 10:10:26 -0400

    add HexInt class

---
+++ src/simplifier/types/HexInt.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021. Aaron Metsch
+ */
+
+package simplifier.types;
+
+public class HexInt extends Number{
+
+    private long val;
+
+    public HexInt(String a){
+        super();
+        this.val = Long.valueOf(a);
+    }
+    public HexInt(Number a){
+        super();
+        this.val = a.longValue();
+    }
+
+    public String toString(){
+        return Long.toHexString(this.val);
+    }
+
+    @Override
+    public int intValue() {
+        return (int) this.val;
+    }
+
+    @Override
+    public long longValue() {
+        return this.val;
+    }
+
+    @Override
+    public float floatValue() {
+        return this.val;
+    }
+
+    @Override
+    public double doubleValue() {
+        return this.val;
+    }
+}
