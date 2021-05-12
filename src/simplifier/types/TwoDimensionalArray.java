2c851ffc99890cb238278c195403f3688f2f9c22
Aaron <aaron1@metschnet.com>
2021-05-12 09:09:42 -0400

    add TwoDimensionalArray class

---
+++ src/simplifier/types/TwoDimensionalArray.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021. Aaron Metsch
+ */
+
+package simplifier.types;
+
+import java.util.ArrayList;
+
+public class TwoDimensionalArray<E>{
+
+    private E[][] arr;
+    private int rows, cols;
+
+    public TwoDimensionalArray(E[][] arr){
+        this.arr = arr;
+    }
+
+    public E get(int row, int col) {
+        return arr[row][col];
+    }
+
+    public int size() {
+        return rows*cols;
+    }
+
+    public void set(E e, int row, int col){
+        arr[row][col] = e;
+    }
+
+    public E[][] toArray(){
+        E[][] temp = arr;
+        return temp;
+    }
+
+    public void setArr(E[][] arr){
+        this.arr = arr;
+    }
+
+    public ArrayList<E> asArrayList(){
+        ArrayList<E> temp = new ArrayList<>();
+        for(int i = 0; i < this.rows; i++){
+            for (int j = 0; j < this.cols; j++) {
+                temp.add(arr[i][j]);
+            }
+        }
+        return temp;
+    }
+}
